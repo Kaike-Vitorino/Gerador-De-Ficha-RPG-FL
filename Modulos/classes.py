966fcb94cfb7281055a34cc61a07debc3fc027fd
KaikeVitorino <kaikevoliveira@gmail.com>
2023-10-24 23:25:48 -0300

    Bug no commit anterior. Ocorreu um bug com o modulo classes no commit anterior. Isso ja foi corrigido.

---
+++ Modulos/classes.py
@@ -0,0 +1,124 @@
+from itens import *
+
+'''
+   Modulo com todas informacoes de todas as classes.
+'''
+
+# Lista q contem as todas as classes.
+classes = ["Caçador", "Druida", "Mago", "Rider", "Guerreiro", "Ladino", "Mascate", "Bardo"]
+
+# Infos da classe/profissoes.
+classe_info = {
+    "Caçador": {
+        "atributo_chave": "Agilidade",
+        "pericias": ["Furtividade", "Movimentação", "Pontaria", "Patrulha", "Sobrevivência"],
+        "equipamentos": {
+            "Arma": ["Arco", "Funda"],
+            "Armadura": None,
+            "Itens": 2,
+            "Artefato Musical": None,
+            "Cavalo": 0
+        },
+        "dados_recurso": {"Comida": "D8", "Água": "D8", "Flechas": "D10", "Prata": "D6"}
+    },
+    "Druida": {
+        "atributo_chave": "Inteligência",
+        "pericias": ["Resiliência", "Sobrevivência", "Discernimento", "Cura", "Adestramento"],
+        "equipamentos": {
+            "Arma": ["Bastão", "Faca"],
+            "Armadura": None,
+            "Itens": 1,
+            "Artefato Musical": None,
+            "Cavalo": 0
+        },
+        "dados_recurso": {"Comida": "D8", "Água": "D8" , "Prata": "D6"}
+    },
+    "Mago": {
+        "atributo_chave": "Inteligência",
+        "pericias": ["Artesanato", "Artimanha", "Tradição", "Discernimento", "Manipulação"],
+        "equipamentos": {
+            "Arma": ["Bastão", "Faca"],
+            "Armadura": None,
+            "Itens": 1,
+            "Artefato Musical": None,
+            "Cavalo": 0
+        },
+        "dados_recurso": {"Comida": "D6", "Água": "D8" , "Prata": "D8"}
+    },
+    "Rider": {
+        "atributo_chave": "Agilidade",
+        "pericias": ["Resiliência", "Luta", "Pontaria", "Sobrevivência", "Adestramento"],
+        "equipamentos": {
+            "Arma": ["Lança Curta", "Machadinha","Arco Curto", "Funda"],
+            "Armadura": None,
+            "Itens": 1,
+            "Artefato Musical": None,
+            "Cavalo": 1
+        },
+        "dados_recurso": {"Comida": "D8", "Água": "D8", "Flechas": "D10", "Prata": "D6"}
+    },
+    "Guerreiro": {
+        "atributo_chave": "Força",
+        "pericias": ["Potência", "Resiliência", "Luta", "Movimentação"],
+        "equipamentos": {
+            "Arma": armas_1m,
+            "Armadura": "Couro",
+            "Itens": 1,
+            "Artefato Musical": None,
+            "Cavalo": 0
+        },
+        "dados_recurso": {"Comida": "D8", "Água": "D6", "Prata": "D6"}
+    },
+    "Ladino": {
+        "atributo_chave": "Agilidade",
+        "pericias": ["Luta", "Furtividade", "Artimanha", "Movimentação", "Manipulação"],
+        "equipamentos": {
+            "Arma": ["Adaga"],
+            "Armadura": None,
+            "Itens": 2,
+            "Artefato Musical": None,
+            "Cavalo": 0
+        },
+        "dados_recurso": {"Comida": "D6", "Água": "D6", "Prata": "D10"}
+    },
+    "Mascate": {
+        "atributo_chave": "Empatia",
+        "pericias": ["Artesanato", "Artimanha", "Patrulha", "Discernimento", "Manipulação"],
+        "equipamentos": {
+            "Arma": ["Faca"],
+            "Armadura": None,
+            "Itens": 3,
+            "Artefato Musical": None,
+            "Cavalo": 0
+        },
+        "dados_recurso": {"Comida": "D8", "Água": "D8", "Prata": "D12"}
+    },
+    "Bardo": {
+        "atributo_chave": "Empatia",
+        "pericias": ["Tradição", "Discernimento", "Manipulação", "Atuação", "Cura"],
+        "equipamentos": {
+            "Arma": ["Faca"],
+            "Armadura": None,
+            "Itens": 1,
+            "Artefato Musical": ["Lira", "flauta"],
+            "Cavalo": 0
+        },
+        "dados_recurso": {"Comida": "D8", "Água": "D6", "Prata": "D8"}
+    }
+}
+
+# Dicionario de talentos das classes.
+talentos_classes = {
+    "Caçador": ["Caminho da Fera", "Caminho da Flecha", "Caminho da Floresta"],
+    "Druida": ["Caminho da Cura", "Caminho da Visão", "Caminho do Metamorfo"],
+    "Mago": ["Caminho da Morte", "Caminho das Rochas", "Caminho do Sangue", "Caminho dos Símbolos"],
+    "Rider": ["Caminho das Planícies", "Caminho do Cavaleiro", "Caminho do Companheiro"],
+    "Guerreiro": ["Caminho da Lâmina", "Caminho do Escudo", "Caminho do Inimigo"],
+    "Ladino": ["Caminho do Matador", "Caminho do Rosto", "Caminho do Veneno"],
+    "Mascate": ["Caminho da Mentira", "Caminho das Muitas Coisas", "Caminho do Ouro"],
+    "Bardo": ["Caminho da Canção", "Caminho do Grito de Guerra", "Caminho do Hino"]
+}
+
+# Dicionario de armas de 1m (que so precisa empunhar com uma mao).
+# Tive que declarar somente ela dessa forma pq o guerreiro eh o unico que usa ela.
+armas_1m_lista = list(lista_todas_armas_1m.keys())
